73d51930e739f3cdadae40a9aba6838f81ca9a29
Naveen <thenvnsahu@gmail.com>
2020-09-03 17:13:15 +0530

    added class for large width navbar

--- src/js/navbar.js
+++ src/js/navbar.js
@@ -13,7 +13,7 @@ class SmallWidthNavbar {
       translateX: "-100%",
       begin: () => {
         for (let label of this.labels) {
-          new ScrambleText(label).scramble();
+          new ScrambleText(label, 100).scramble();
         }
       },
     });
@@ -36,4 +36,50 @@ class SmallWidthNavbar {
   }
 }
 
-export { SmallWidthNavbar };
+class LargeWidthNavbar {
+  constructor(query) {
+    this.elem = document.querySelector(query);
+    this.navLinks = this.elem.querySelectorAll(".js-nav-link");
+    this.current = -1;
+  }
+
+  _playAnim(idx, reverse = false) {
+    const indicatorElem = this.navLinks[idx].querySelector(
+      ".js-nav-link-indicator"
+    );
+    const labelElem = this.navLinks[idx].querySelector(".js-nav-link-label");
+    const numberElem = this.navLinks[idx].querySelector(".js-nav-link-number");
+
+    anime
+      .timeline({
+        easing: "easeOutQuad",
+        duration: 300,
+        autoplay: true,
+        direction: reverse ? "reverse" : "normal",
+      })
+      .add({
+        targets: indicatorElem,
+        width: [5, 15],
+      })
+      .add(
+        {
+          targets: numberElem,
+          opacity: [0.4, 1],
+          begin: () => {
+            new ScrambleText(labelElem).scramble(reverse);
+          },
+        },
+        0
+      );
+  }
+
+  setCurrentNavLink(idx) {
+    if (idx < 0 || idx >= this.navLinks.length) return;
+    // console.log(`Set current nav link ${idx}/${this.navLinks.length}`);
+    if (this.current >= 0) this._playAnim(this.current, true);
+    this._playAnim(idx);
+    this.current = idx;
+  }
+}
+
+export { SmallWidthNavbar, LargeWidthNavbar };
