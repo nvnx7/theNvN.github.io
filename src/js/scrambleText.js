a8c3b3214086cd141edbd43d664c81554e31acab
Naveen <thenvnsahu@gmail.com>
2020-09-03 17:10:35 +0530

    fix: reverse scramble not occuring

--- src/js/scrambleText.js
+++ src/js/scrambleText.js
@@ -1,29 +1,18 @@
 import anime from "animejs/lib/anime.es.js";
 
 export default class ScrambleText {
-  constructor(query) {
+  constructor(query, delay = 0) {
     if (typeof query === "string") this.elem = document.querySelector(query);
     else this.elem = query;
 
-    this.chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    this.chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!$#&%";
+    this.delay = delay;
     this.config = {
-      originalTxt: this.elem.innerText,
+      originalTxt: this.elem.dataset.label,
       count: 0,
-      max: this.elem.innerText.length * 50,
-      duration: this.elem.innerText.length * 40,
+      max: this.elem.dataset.label.length * 50,
+      duration: this.elem.dataset.label.length * 40,
     };
-    this.anim = anime({
-      targets: this.config,
-      duration: this.config.duration,
-      count: this.config.max,
-      round: 1,
-      easing: "linear",
-      autoplay: false,
-      delay: 50,
-      update: () => {
-        this.elem.innerText = this._getRandomText();
-      },
-    });
   }
 
   _getRandomText() {
@@ -36,16 +25,23 @@ export default class ScrambleText {
 
     txt += this.config.originalTxt.slice(0, len);
     txt += this.chars.charAt(Math.floor(Math.random() * this.chars.length));
-
+    console.log(txt);
     return txt;
   }
 
   scramble(reverse = false) {
-    if (!reverse) {
-      if (this.anim.reversed) this.anim.reverse();
-    } else {
-      if (!this.anim.reversed) this.anim.reverse();
-    }
-    this.anim.play();
+    anime({
+      targets: this.config,
+      duration: this.config.duration,
+      count: [0, this.config.max],
+      round: 1,
+      easing: "linear",
+      autoplay: true,
+      delay: this.delay,
+      direction: reverse ? "reverse" : "normal",
+      update: () => {
+        this.elem.innerText = this._getRandomText();
+      },
+    });
   }
 }
