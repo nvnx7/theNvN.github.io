a1220ed8b1e3b9a897793552ee1501345b977efa
Naveen <thenvnsahu@gmail.com>
2020-09-15 00:06:41 +0530

    added clean-webpack plugin dependency

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
@@ -62,5 +63,6 @@ module.exports = {
       filename: "index.html",
       template: path.resolve(__dirname, "src", "index.html"),
     }),
+    new CleanWebpackPlugin(),
   ],
 };
