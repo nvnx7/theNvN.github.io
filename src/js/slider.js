0ceee9af9b7e143f22204798e05c4d548a103738
Naveen <thenvnsahu@gmail.com>
2020-10-05 00:12:57 +0530

    refactored for prestation animation

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,5 +1,5 @@
 import anime from "animejs/lib/anime.es.js";
-import { ScrambleTexts } from "./scrambleText";
+import { ScrambleText, ScrambleTexts } from "./scrambleText";
 
 class Slider {
   constructor(query) {
@@ -9,12 +9,14 @@ class Slider {
     this.icons = this.elem.querySelector(".js-skill-icons");
     this.index = this.elem.querySelector(".js-skill-index-container");
     this.tools = this.elem.querySelectorAll(".js-skill-tools-container");
+    this.animating = false;
     this.current = 0;
   }
 
   moveTo(idx) {
     console.log(`Slider moveto ${idx}`);
     if (idx < 0) return;
+    this.animating = true;
     this._animateTitle(idx);
     this._animateText(idx);
     this._animateTools(idx);
@@ -22,12 +24,35 @@ class Slider {
     this._animateIndex(idx);
   }
 
-  _animateTitle(idx) {
-    this.titlesAnim.scrambleTo(idx);
+  animatePresentation(idx, reverse = false) {
+    this.animating = true;
+    const direction = reverse ? "reverse" : "normal";
+    anime({
+      targets: this.elem.querySelector(".js-skill-desc"),
+      width: [0, "50%"],
+      duration: 800,
+      easing: "easeOutCubic",
+      direction: reverse ? "reverse" : "normal",
+    });
+    this._animateTitle(idx, { direction });
+    this._animateText(idx, { delay: 500, direction });
+    this._animateIndex(idx, { delay: 500, direction });
+    this._animateIcon(idx, { direction });
+    this._animateTools(idx, { direction });
+    this._animateButton({ direction });
   }
 
-  _animateText(idx) {
+  _animateTitle(idx, options = {}) {
+    if (options.direction == "reverse") {
+      new ScrambleText(".js-skill-title").scramble(true);
+    } else {
+      this.titlesAnim.scrambleTo(idx);
+    }
+  }
+
+  _animateText(idx, options = {}) {
     anime({
+      ...options,
       targets: this.texts.children[idx],
       duration: 450,
       easing: "easeOutCubic",
@@ -44,9 +69,10 @@ class Slider {
     });
   }
 
-  _animateIcon(idx) {
+  _animateIcon(idx, options = {}) {
     anime
       .timeline({
+        ...options,
         duration: 250,
         easing: "easeOutCubic",
       })
@@ -68,10 +94,11 @@ class Slider {
       });
   }
 
-  _animateTools(idx) {
+  _animateTools(idx, options = {}) {
     console.log(this.tools[this.current]);
     anime
       .timeline({
+        ...options,
         duration: 450,
         easing: "easeOutCubic",
       })
@@ -86,16 +113,17 @@ class Slider {
       });
   }
 
-  _animateIndex(idx) {
+  _animateIndex(idx, options = {}) {
     anime
       .timeline({
+        ...options,
         duration: 250,
         easing: "easeOutCubic",
       })
       .add({
         targets: this.index,
         duration: 500,
-        width: ["5%", "35%"],
+        width: [0, "35%"],
       })
       .add(
         {
@@ -104,9 +132,21 @@ class Slider {
           begin: () => {
             this.index.firstElementChild.textContent = `0${idx + 1}`;
           },
+          complete: () => {
+            this.animating = false;
+          },
         },
         0
       );
+  }
+
+  _animateButton(options = {}) {
+    anime({
+      ...options,
+      targets: this.elem.querySelector(".js-skills-back-btn"),
+      duration: 250,
+      opacity: [0, 1],
+    });
   }
 }
 
@@ -119,49 +159,113 @@ class SliderNavbar {
     this.indexElem = this.elem.querySelector(".js-nav-index");
     this.currentElem = this.indexElem.querySelector(".js-current-index");
     this.total = Math.floor(Number(this.elem.dataset.total));
-    this.isAnimating = false;
+    this.animating = false;
     this.current = 0;
 
-    this.moveTo(this.current);
-
     this.checkpointsElem.addEventListener("click", (e) => {
+      if (this.isAnimating()) return;
       const n = Math.floor(Number(e.target.dataset.checkpoint)) ?? -1;
-      console.log(`Chkpoint- ${n}`);
       if (isNaN(n)) return;
       if (n >= 0 && n <= this.total - 1) {
         this.moveTo(n);
+        this.slider?.moveTo(n);
       }
     });
 
     this.elem.querySelector(".js-btn-prev").addEventListener("click", () => {
-      if (this.current <= 0) return;
+      if (this.current <= 0 || this.isAnimating()) return;
       this.prev();
     });
 
     this.elem.querySelector(".js-btn-next").addEventListener("click", () => {
-      if (this.current >= this.total - 1) return;
+      if (this.current >= this.total - 1 || this.isAnimating()) return;
       this.next();
-      console.log(`Current: ${this.current}/${this.total - 1}`);
     });
   }
 
+  isAnimating() {
+    return this.slider?.animating || this.animating;
+  }
+
+  animateIn(idx, animCallbacks = {}) {
+    this._animatePresentation(idx, false, animCallbacks);
+  }
+
+  animateOut(animCallbacks = {}) {
+    this._animatePresentation(this.current, true, animCallbacks);
+  }
+
+  _animatePresentation(idx, reverse = false, animCallbacks = {}) {
+    anime
+      .timeline({
+        easing: "easeOutQuad",
+        duration: 500,
+        direction: reverse ? "reverse" : "normal",
+      })
+      .add(
+        {
+          targets: [
+            this.barElem,
+            this.elem.querySelector(".js-btn-prev"),
+            this.elem.querySelector(".js-btn-next"),
+          ],
+          opacity: [0, 1],
+          begin: () => {
+            this.indexElem.style.transform = `translateY("100%")`;
+            if (animCallbacks.begin) animCallbacks.begin();
+          },
+          complete: () => {
+            this.setTo(idx);
+          },
+        },
+        0
+      )
+      .add({
+        targets: this.indexElem,
+        translateY: ["100%", 0],
+        complete: () => {
+          if (animCallbacks.complete) animCallbacks.complete();
+        },
+      });
+
+    this.slider?.animatePresentation(idx, reverse);
+  }
+
   next() {
-    this.moveTo(this.current + 1, 1);
+    if (this.isAnimating()) return;
+    const n = this.current + 1;
+    this.moveTo(n);
+    this.slider?.moveTo(n);
   }
 
   prev() {
-    this.moveTo(this.current - 1, -1);
+    if (this.isAnimating()) return;
+    const n = this.current - 1;
+    this.moveTo(n);
+    this.slider?.moveTo(n);
   }
 
-  moveTo(n) {
-    if (this.isAnimating) return;
+  setTo(n) {
     if (n < 0 || n >= this.total) return;
     this.current = n;
     const offset = this.indexElem.getBoundingClientRect().width / 2;
     const trackW = this.trackElem.getBoundingClientRect().width;
     const leftPos = trackW * (n / (this.total - 1)) - offset;
     const leftPercent = (leftPos / trackW) * 100;
-    this.isAnimating = true;
+    console.log(`Data - ${offset}, ${trackW}, ${leftPos}, ${leftPercent}`);
+    this.currentElem.textContent = `0${n + 1}`;
+    this.indexElem.style.left = `${leftPercent}%`;
+  }
+
+  moveTo(n) {
+    if (this.isAnimating()) return;
+    if (n < 0 || n >= this.total) return;
+    this.current = n;
+    const offset = this.indexElem.getBoundingClientRect().width / 2;
+    const trackW = this.trackElem.getBoundingClientRect().width;
+    const leftPos = trackW * (n / (this.total - 1)) - offset;
+    const leftPercent = (leftPos / trackW) * 100;
+    this.animating = true;
 
     anime({
       targets: this.indexElem,
@@ -190,11 +294,9 @@ class SliderNavbar {
         duration: 250,
         translateY: ["100%", 0],
         complete: () => {
-          this.isAnimating = false;
+          this.animating = false;
         },
       });
-
-    this.slider?.moveTo(this.current);
   }
 
   attachSlider(slider) {
